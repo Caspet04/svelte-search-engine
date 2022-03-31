4451f6124c8d591e708b07c41a0d21a0931beb57
Caspet04 <caspet@edu.umea.se>
2022-03-31 15:37:59 +0200

    Added basic comments to [App.svelte]

--- src/App.svelte
+++ src/App.svelte
@@ -10,13 +10,16 @@
 	<SearchForm bind:result={result}/>
 
 	{#await $result}
+		<!-- Displayed while waiting for a result -->
 		Wait
 	{:then result}
+		<!-- Displayed once a result has been found, if it is a "legal" result -->
 		{#if result && "data" in result && "items" in result.data}
 			<ResultList items={result.data.items} />
 			{console.log(result.data.items)}
 		{/if}
 	{:catch error}
+		<!-- Displayed if an error has occurred -->
 		<h1>An <span style="color:red;">Error</span> Has Ocurred!</h1>
 		{JSON.stringify(error)}
 	{/await}
